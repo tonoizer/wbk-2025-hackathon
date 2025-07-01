5475ed9f3892694445bf312958971ce56d8c97b3
Kevin Beier <hey@kevinbeier.com>
2025-07-01 22:02:11 +0200

    adjusted github urls

--- app/config/navigation.ts
+++ app/config/navigation.ts
@@ -21,7 +21,7 @@ export const socialItemsWithOutLabel = [
   },
   {
     icon: 'i-simple-icons-github',
-    to: 'https://github.com/BeierKevin/KIT-Hackathon',
+    to: 'https://github.com/Beierthon/frontend',
     target: '_blank',
   },
 ]
@@ -36,7 +36,7 @@ export const socialItemsWithLabel = [
   {
     label: 'GitHub',
     icon: 'i-simple-icons-github',
-    to: 'https://github.com/BeierKevin/KIT-Hackathon',
+    to: 'https://github.com/Beierthon/frontend',
     target: '_blank',
   },
 ]
